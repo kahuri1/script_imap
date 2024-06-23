1f0e1bd4714191969796886f6d17b614824bb52b
kahuri1 <vova150820@gmail.com>
2024-06-23 05:00:12 +0300

    v_1.2 pull message

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/emersion/go-imap"
+	"github.com/emersion/go-message/mail"
 	"github.com/sirupsen/logrus"
+	"io"
 	"io/ioutil"
 	"log"
 	"time"
@@ -47,6 +50,13 @@ func main() {
 
 		messages := make(chan *imap.Message, 1)
 		done := make(chan error, 1)
+		section := &imap.BodySectionName{
+			/* BodyPartName: imap.BodyPartName{
+				Specifier: imap.MIMESpecifier,
+				Fields:    []string{"X-Priority", "Priority"},
+			}, */
+		}
+
 		go func() {
 			done <- c.Fetch(seqset, []imap.FetchItem{sect.FetchItem(), imap.FetchEnvelope}, messages)
 		}()
@@ -56,35 +66,88 @@ func main() {
 				continue
 			} else {
 				log.Println("* " + msg.Envelope.Subject)
-				for _, p := range msg.Body {
 
-					data, err := ioutil.ReadAll(p) // Читаем содержимое файла в []byte
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					//TODO тут нам нужно разобраться с расширением adsda
-					err = ioutil.WriteFile("output.html", data, 0644)
-					if err != nil {
-						log.Fatal(err)
-					}
-
-				}
-
-				lastIUD = msg.Envelope.MessageId
-				from++
-
-				err := SetDefaultValue(from, lastIUD)
+				mr, err := mail.CreateReader(msg.GetBody(section))
 				if err != nil {
-					logrus.Fatalf("error setting default from: %s", err.Error())
+					log.Fatal(err)
 				}
+
+				// Print some info about the message
+				header := mr.Header
+				if date, err := header.Date(); err == nil {
+					log.Println("Date:", date)
+				}
+				if from, err := header.AddressList("From"); err == nil {
+					log.Println("From:", from)
+				}
+				if to, err := header.AddressList("To"); err == nil {
+					log.Println("To:", to)
+				}
+
+				if prio := header.Get("X-Priority"); prio != "" {
+					log.Println("Priority:", prio)
+				}
+
+				if subject, err := header.Subject(); err == nil {
+					log.Println("Subject:", subject)
+				}
+
+				if conxtdis := header.Get("Content-Disposition"); conxtdis != "" {
+					log.Println("Content-Disposition:", conxtdis)
+				}
+
+				// Process each message's part
+				for {
+					p, err := mr.NextPart()
+					if err == io.EOF {
+						break
+					} else if err != nil {
+						log.Fatal(err)
+					}
+
+					disp := p.Header.Get("Content-Disposition")
+
+					switch h := p.Header.(type) {
+					case *mail.InlineHeader:
+						// This is the message's text (can be plain-text or HTML)
+						b, _ := ioutil.ReadAll(p.Body)
+						if disp != "" {
+
+							contentID := h.Get("Content-ID")
+
+							_, pr, _ := h.ContentType()
+
+							filename := fmt.Sprintf("%s.%s", contentID, pr["name"])
+
+							ioutil.WriteFile(filename, b, 0777)
+
+						} else {
+							log.Printf("Got ====: %s", string(b))
+						}
+					case *mail.AttachmentHeader:
+
+						log.Printf("Got attachment==========")
+						// This is an attachment
+						filename, _ := h.Filename()
+
+						log.Printf("Got attachment: %v", filename)
+						b, errp := ioutil.ReadAll(p.Body)
+						fmt.Println("errp ===== :", errp)
+						err := ioutil.WriteFile(filename, b, 0777)
+
+						if err != nil {
+							log.Println("attachment err: ", err)
+						}
+					}
+				}
+
 			}
 		}
-
 		if err := <-done; err != nil {
 			log.Fatal(err)
 		}
 
 		time.Sleep(time.Second * 5)
+
 	}
 }
